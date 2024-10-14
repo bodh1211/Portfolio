c8c0e8da6023c6a2fd2d1b26980c924b092d5d6a
bodh1211 <bodhratan1211@gmail.com>
2024-10-15 00:08:41 +0530

    Add files via upload
    Files to my Portfolio website.

---
+++ script.js
@@ -0,0 +1,11 @@
+// Script to add interactivity (if needed in future enhancements)
+// For example, you can add event listeners for smooth scrolling
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
